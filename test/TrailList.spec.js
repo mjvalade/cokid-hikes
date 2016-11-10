daf4acb3d6e112b0b559be16d66f02d0d0ad9b98
mjvalade <mjvalade@gmail.com>
2016-11-09 19:57:14 -0700

    beginnings of jest tests

--- test/TrailList.spec.js
+++ test/TrailList.spec.js
@@ -4,13 +4,15 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import { assert, expect } from 'chai';
 
-import Application from '../lib/components/Application';
+import TrailList from '../lib/components/TrailList';
 
-describe('Application', () => {
+describe('Trail List', () => {
   it('renders as a <div>', () => {
-    const wrapper = shallow(<Application />);
+    const wrapper = shallow(<TrailList />);
     assert.equal(wrapper.type(), 'div');
   });
 
+  it('', () => {
 
+  });
 });
---
+++ test/actions/actions.test.js
@@ -0,0 +1,35 @@
+/* globals describe, it, expect */
+
+import * as actions from '../../actions';
+
+import { startListeningToAuth, logIn, logOut } from  '../../actions/auth';
+import { createTrail, fetchAllTrails, setSelectedTrail, receiveCurrentLocalWeather, fetchLocalWeather } from  '../../actions';
+
+
+describe('Auth actions', () => {
+  it('should create an action', () => {
+    expect(true).toEqual(true);
+  });
+
+  xit('should create an action to start listening for login', () => {
+
+  });
+
+
+});
+
+describe('Main actions', () => {
+  it('should create an action', () => {
+    expect(true).toEqual(true);
+  });
+
+  it('should create an action to create a new trail', () => {
+    const trailData = 'Flower trail';
+    const expectedAction = {
+      type: 'CREATE_NEW_TRAIL',
+      trail: trailData
+    };
+
+    expect(createTrail(trailData)).toEqual(expectedAction);
+  });
+});
